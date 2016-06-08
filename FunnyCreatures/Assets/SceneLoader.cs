1af1bf0ffdb21890ac06fc88d53b0e0127f9e2f6
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-06-08 09:28:19 +0200

    Button Reload, Sun shafts

---
+++ FunnyCreatures/Assets/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
